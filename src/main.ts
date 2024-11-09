01215670a334b79dd41b979737671c2a7f42d99c
Timi2424 <tymonbieganski@wp.pl>
2024-11-09 23:07:34 +0100

    port heroku fix

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,6 @@ async function bootstrap() {
     SwaggerModule.setup('', app, document);
 
     app.use(cookieParser());
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
