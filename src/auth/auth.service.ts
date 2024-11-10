6fa7553be54a7263d3879b796706640b44f30177
Timi2424 <tymonbieganski@wp.pl>
2024-11-10 19:01:57 +0100

    fix fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import logger from 'src/utils/logger';
+import logger from '../utils/logger';
 
 @Injectable()
 export class AuthService {
