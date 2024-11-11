279ce6340f8121ba89031f39ecd1950d0b163c25
Timi2424 <tymonbieganski@wp.pl>
2024-11-11 17:34:01 +0100

    false stsate

--- src/auth/auth.strategy.ts
+++ src/auth/auth.strategy.ts
@@ -11,6 +11,7 @@ export class Auth0Strategy extends PassportStrategy(Strategy) {
       clientID: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       callbackURL: process.env.AUTH0_CALLBACK_URL,
+      state: false
     });
   }
 
