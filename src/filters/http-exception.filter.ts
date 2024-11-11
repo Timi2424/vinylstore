7a1254ee509ccacf12e63fdfd9dbc7c0f245d735
Timi2424 <tymonbieganski@wp.pl>
2024-11-11 16:54:51 +0100

    bibi

--- src/filters/http-exception.filter.ts
+++ src/filters/http-exception.filter.ts
@@ -24,7 +24,7 @@ import { systemLogger } from 'src/utils/logger';
       const message =
         exception instanceof HttpException
           ? exception.getResponse()
-          : 'Internal server error';
+          : 'Internal bibi error';
   
       systemLogger.log(
         `HTTP Status: ${status} Error Message: ${JSON.stringify(message)}`,
