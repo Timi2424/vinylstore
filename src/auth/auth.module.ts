d9f4c4790e56567621d83a04cabfc2093adba0ef
Timi2424 <tymonbieganski@wp.pl>
2024-11-13 20:34:20 +0100

    auth service fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule } from '@nestjs/config';
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { Auth0Strategy } from './auth.strategy';
 import { UserModule } from '../user/user.module';
@@ -13,8 +12,8 @@ import { SessionSerializer } from './serialization';
     PassportModule.register({session: true}),
     forwardRef(() => UserModule)
   ],
-  providers: [AuthService, Auth0Strategy, SessionSerializer],
+  providers: [Auth0Strategy, SessionSerializer],
   controllers: [AuthController],
-  exports: [AuthService],
+  exports: [],
 })
 export class AuthModule {}
